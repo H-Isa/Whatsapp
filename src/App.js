1703332720d7e9cb7ea7b2e826d14b4e032bcd89
H-Isa <emjay296@yahoo.com>
2023-02-22 23:36:52 +0300

    remove boiler plate and add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from 'react';
+import Container from './Container';
+import Modal from './Modal';
 
 function App() {
+
+  const [showModal, setShowModal] = useState(true);
+  const [enteredUsername, setEnteredUsername] = useState('');
+
+  const handleModal = (e) => {
+    e.preventDefault();
+    setShowModal(false)
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {showModal ? <Modal onHandleModal={handleModal} 
+                          enteredUsername={enteredUsername} 
+                          setEnteredUsername={setEnteredUsername}/> : <Container enteredUsername={enteredUsername} 
+                                                                                 setEnteredUsername={setEnteredUsername} />}
+    </Fragment>
   );
 }
 
